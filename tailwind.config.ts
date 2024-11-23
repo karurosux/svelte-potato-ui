f5cdeb92669090304702ebae31903aa51a914d5d
Carlos Gonzalez <cgonzalez@semantic-ai.com>
2024-11-22 21:42:28 -0800

    Building css and include it in the package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,8 @@ import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
 		extend: {}
 	},
-
 	plugins: [typography]
 } satisfies Config;
