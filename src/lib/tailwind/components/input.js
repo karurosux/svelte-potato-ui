80406230abc72bdca0b757102bca96d68f66990a
Carlos Gonzalez <justdevelopitmx@gmail.com>
2025-03-18 00:42:51 -0700

    changed colors of input

--- src/lib/tailwind/components/input.js
+++ src/lib/tailwind/components/input.js
@@ -1,16 +1,16 @@
 const { getBaseColorsFromTheme } = require('./get-base-colors-from-theme');
 
 const inputComponent = (/** @type {(arg0: string) => any} */ theme) => {
-	const { base1, foreground } = getBaseColorsFromTheme(theme);
+	const { base1, foreground, contrast, text } = getBaseColorsFromTheme(theme);
 
 	return {
 		'.input': {
 			'@apply rounded-lg border-2 p-4 bg-transparent px-8 text-lg font-bold outline-none focus:border-sky-300 focus:outline-none focus:ring-1 focus:ring-sky-300 focus:invalid:border-pink-500 focus:invalid:ring-pink-500':
 				{},
-			borderColor: foreground,
-			color: foreground,
+			borderColor: contrast,
+			color: text,
 			'&::placeholder': {
-				color: foreground,
+				color: text,
 				opacity: 0.6
 			},
 			option: {
